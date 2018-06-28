29e9bab8570986c589cb4f9f8050bf889a9c88a1
Zuzana Bumbova <z.bumbova@gmail.com>
2018-06-28 11:20:55 +0200

    detail

--- src/main/java/cz/pavi/ocka/el_jidelni_listek/Help.java
+++ src/main/java/cz/pavi/ocka/el_jidelni_listek/Help.java
@@ -14,7 +14,7 @@ public class Help {
     
      public static String getHelp(){
     
-    String tutorial = "Vitej u napovedy E-Jidelniho listku \n Jidlo si objednas tim, ze si jej najdes v seznamu a stisknes tlacitko Pridat k objednavce \n K nekterym jidlum si muzes vybrat prilohu \n Vedle tlacika objednat ti naskakuje celkova cena objednavky \n Po pridani vsech jidel k objednavce stistkni tlacitko Objednat  ";
+    String tutorial = "Vitej u napovedy E-Jidelniho listku \n Jidlo si objednas tim, ze si jej najdes v seznamu a stisknes tlacitko Pridat k objednavce \n K nekterym jidlum si muzes vybrat prilohu \n Vedle tlacika objednat se zobrazuje celkova cena objednavky \n Po pridani vsech jidel k objednavce stistkni tlacitko Objednat  ";
         
     
     return tutorial;
