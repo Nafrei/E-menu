b254404d0ce24982f4caa07f4812e58efda007b2
Zuzana Bumbova <z.bumbova@gmail.com>
2018-06-28 18:28:16 +0200

    třída meal

--- UAI_pub-client/src/main/java/cz/pavi/ocka/el_jidelni_listek/Meal.java
+++ UAI_pub-client/src/main/java/cz/pavi/ocka/el_jidelni_listek/Meal.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cz.pavi.ocka.el_jidelni_listek;
 
 import javafx.scene.image.Image;
 
-/**
- *
- * @author Langi
- */
 public class Meal {
-    
+
     private String mealName;
     private String description;
     private String allergens;
@@ -20,9 +11,6 @@ public class Meal {
     private int type;
     private Image picture;
     private int price;
-   
-  
-    
 
     public Meal(String name, String description, String allergens, String quantity, int price, int type, Image picture) {
         this.mealName = name;
@@ -33,54 +21,42 @@ public class Meal {
         this.type = type;
         this.price = price;
     }
-    
-    public Meal(String name, int price)
-    {
+
+    public Meal(String name, int price) {
         this.mealName = name;
         this.price = price;
     }
-    
-    public String getName()
-    {
+
+    public String getName() {
         return mealName;
     }
-    
-    public String getDescription()
-    {
+
+    public String getDescription() {
         return description;
     }
-    
-    public String getAllergens()
-    {
+
+    public String getAllergens() {
         return allergens;
     }
-    
-    public String getQuantity()
-    {
+
+    public String getQuantity() {
         return quantity;
     }
 
     public void setQuantity(String quantity) {
         this.quantity = quantity;
     }
-    
-    
-    public int getPrice()
-    {
+
+    public int getPrice() {
         return price;
     }
-    
-    public Image getPicture()
-    {
+
+    public Image getPicture() {
         return picture;
     }
-    
-    public int getType()
-    {
+
+    public int getType() {
         return type;
     }
-    
-    
 
-    
 }
