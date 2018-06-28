12ea4809bde0de0b0c83b5a267bfce36ba88e139
Zuzana Bumbova <z.bumbova@gmail.com>
2018-06-28 18:23:49 +0200

    formátování a komentáře

--- UAI_pub-client/src/main/java/cz/pavi/ocka/el_jidelni_listek/MealServiceImplementation.java
+++ UAI_pub-client/src/main/java/cz/pavi/ocka/el_jidelni_listek/MealServiceImplementation.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cz.pavi.ocka.el_jidelni_listek;
 
 import java.util.ArrayList;
@@ -11,55 +6,47 @@ import java.util.List;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
-/**
- *
- * @author Langi
- */
 public class MealServiceImplementation implements MealService {
-    
+
     DatabaseHelper dt;
-    
+
     int chosenTable = 0;
-    
+
     int numberOfOrderedMeals = 0;
-    
+
     private HashMap<Meal, Meal> chosenMeals = null;
-    
+
     private List<Meal> currentMeals = null;
-    
+
     private List<Meal> sideDishes = null;
-    
+
     /**
-     * Konstruktor. Vytvoří novou instanci Databazovniku.
+     * Constructor. Creates a new instance of Databazovnik.
      */
-    public MealServiceImplementation()
-    {
+    public MealServiceImplementation() {
         dt = new DatabaseHelper();
         chosenMeals = new HashMap<Meal, Meal>();
     }
 
-
     @Override
-    public List<Meal> getSideDishes() 
-    {
+    public List<Meal> getSideDishes() {
         List<Meal> databaseList = dt.nactiDataZDatabaze(5);
-        
-        
-        
+
         return databaseList;
     }
-    
-    
+
     /**
-     * Požádá DatabaseHelper o hodnoty z databáze k danému typu jídla a vrátí je jako ObservableList
-     * @param type Typ jídla
-     * @return 
+     * Asks DatabaseHelper for values of given type of food from the database
+     * and returns them as ObservableList.
+     *
+     * @param type type of food
+     * @return
      */
     @Override
     public List<Meal> getCurrentMeals(int type) {
         List<Meal> databaseList = dt.nactiDataZDatabaze(type);
-        currentMeals = FXCollections.observableArrayList (databaseList);
-        
+        currentMeals = FXCollections.observableArrayList(databaseList);
+
         return currentMeals;
     }
 
@@ -88,7 +75,6 @@ public class MealServiceImplementation implements MealService {
         return chosenMeals;
     }
 
-
     @Override
     public int getNumberOfOrderedMeals() {
         return chosenMeals.size();
@@ -96,23 +82,20 @@ public class MealServiceImplementation implements MealService {
 
     @Override
     public void deleteOrders(int tableID) {
-        
+
     }
 
     @Override
     public int getPriceOfChosenMeals() {
         int sum = 0;
-        for(Meal j: chosenMeals.keySet())
-        {
+        for (Meal j : chosenMeals.keySet()) {
             sum += j.getPrice();
-            if(chosenMeals.get(j) != null)
-            {
-                sum+= chosenMeals.get(j).getPrice();
+            if (chosenMeals.get(j) != null) {
+                sum += chosenMeals.get(j).getPrice();
             }
         }
-        
-        
+
         return sum;
     }
-    
+
 }
