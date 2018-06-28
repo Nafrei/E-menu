311bfea87cfe9ae4679a8f6f7e15280122373486
Petr Voldan <voldysek1@seznam.cz>
2018-06-28 10:06:11 +0200

    napoveda

--- src/main/java/cz/pavi/ocka/el_jidelni_listek/Help.java
+++ src/main/java/cz/pavi/ocka/el_jidelni_listek/Help.java
@@ -13,7 +13,7 @@ public class Help {
     
      public static String getHelp(){
     
-    String tutorial = "Vitej u napovedy E-Jidelniho listku \n Jidlo si objednas tim, ze si jej vyberes v seznamu a zmacknes tlacitko ... ";
+    String tutorial = "Vitej u napovedy E-Jidelniho listku \n Jidlo si objednas tim, ze si jej najdes v seznamu a zmacknes tlacitko Pridat k objednavce \n K nekterym jidlum si muzes vybrat prilohu \n Vedle tlacika objednat ti naskakuje celkova cena objednavky \n Po pridani vsech jidel k objednavce stistkni tlacitko Objednat  ";
         
     
     return tutorial;
