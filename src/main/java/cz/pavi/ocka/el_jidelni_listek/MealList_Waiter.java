47b4c8c9af2841b6525f7c8e2ebfdb2c788a88ce
Petr Voldan <voldysek1@seznam.cz>
2018-06-28 09:00:13 +0200

    Martin nas donutil sloucit to do jedne aplikace, vsechny tridy z druhe aplikace se jmenuji ..._Waiter, pry se to tim i zjednodusi, treba Meal bude jen jedno ale ja to nezvladnu spojit, to musi udelat nekdo fundovanejsi

---
+++ src/main/java/cz/pavi/ocka/el_jidelni_listek/MealList_Waiter.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.pavi.ocka.el_jidelni_listek;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author voldy
+ */
+public class MealList_Waiter {
+    
+    private ArrayList<Meal_Waiter> meallist;
+    
+    public MealList_Waiter(){
+        
+        
+        //new ArrayList<Meal>;
+    
+    }
+    
+    public void addToMealList(Meal_Waiter meal){
+    
+    meallist.add(meal);
+    
+    
+    }
+    public void listMealList(){
+   
+        //vypis celeho listu do gui
+        for (int i = 0; i < meallist.size(); i++) {
+           //neco (meallist.get(i);)
+            //vypis polozky do gui
+            
+        }
+       
+    
+    }
+    
+        
+   
+   
+    
+    
+    
+}
