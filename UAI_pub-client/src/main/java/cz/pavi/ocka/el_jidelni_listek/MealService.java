3fffa9134b38e559884d04791059d73b78139cec
Zuzana Bumbova <z.bumbova@gmail.com>
2018-06-28 18:27:32 +0200

    další třída

--- UAI_pub-client/src/main/java/cz/pavi/ocka/el_jidelni_listek/MealService.java
+++ UAI_pub-client/src/main/java/cz/pavi/ocka/el_jidelni_listek/MealService.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cz.pavi.ocka.el_jidelni_listek;
 
 import java.util.ArrayList;
@@ -10,33 +5,27 @@ import java.util.HashMap;
 import java.util.List;
 import javafx.collections.ObservableList;
 
-/**
- *
- * @author Langi
- */
-public interface MealService 
-{
+public interface MealService {
+
     List<Meal> getSideDishes();
-    
+
     List<Meal> getCurrentMeals(int type);
-    
+
     HashMap<Meal, Meal> getChosenMeals();
-    
+
     //List<Order> getOrdersByTableId(int tableId);
-    
     void addToOrder(Meal meal, Meal sideDishes);
-    
+
     void deleteOrders(int tableID);
-    
+
     void makeOrder(int tableID, List<Meal> orderedMeals);
-    
-    void setChosenTable(int number); 
-    
+
+    void setChosenTable(int number);
+
     int getChosenTable();
 
     int getNumberOfOrderedMeals();
-    
+
     int getPriceOfChosenMeals();
-    
-    
+
 }
