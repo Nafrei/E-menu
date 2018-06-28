1df22e128423361e10334c64254ee9f325683abe
Patrik Trochta <rudovouscz@gmail.com>
2018-06-28 23:31:56 +0200

    meal update

--- UAI_pub-client/src/main/java/cz/pavi/ocka/el_jidelni_listek/MealServiceImpl.java
+++ UAI_pub-client/src/main/java/cz/pavi/ocka/el_jidelni_listek/MealServiceImpl.java
@@ -49,7 +49,7 @@ public class MealServiceImpl implements MealService {
 
     @Override
     public void makeOrder(int tableID, List<Meal> orderedMeals) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
