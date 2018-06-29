d9dc75e20754e96b600c2b9e9eba53dd9d81561a
Zuzana Bumbova <z.bumbova@gmail.com>
2018-06-29 09:54:28 +0200

    dokumentace

--- UAI_pub-client/src/main/java/cz/pavi/ocka/el_jidelni_listek/Help.java
+++ UAI_pub-client/src/main/java/cz/pavi/ocka/el_jidelni_listek/Help.java
@@ -1,7 +1,17 @@
 package cz.pavi.ocka.el_jidelni_listek;
 
-public class Help {
+/**
+ * Tutorial on how to use the app. 
+ * Not yet implemented but available for the future.
+ * 
+ */
 
+public class Help {
+/**
+ * Creates text tutorial on how to use the app.
+ * @return String tutorial information about how to use the app
+ */
+    
     public static String getHelp() {
 
         String tutorial = "Vitej u napovedy E-Jidelniho listku \n Jidlo si objednas tim, ze si jej najdes v seznamu a zmacknes tlacitko Pridat k objednavce \n K nekterym jidlum si muzes vybrat prilohu \n Vedle tlacika objednat ti naskakuje celkova cena objednavky \n Po pridani vsech jidel k objednavce stistkni tlacitko Objednat  ";
